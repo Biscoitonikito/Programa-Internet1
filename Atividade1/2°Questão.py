c88ea7283b366435d1c8ae4141cdc8552b09ad85
Biscoitonikito <44701885+Biscoitonikito@users.noreply.github.com>
2019-03-26 10:18:30 -0300

    Segunda Questão
    Concluida

--- Atividade1/2°Questão.py
+++ Atividade1/2°Questão.py
@@ -1,4 +1,3 @@
-import urllib.request
 import requests
 
 class Download:
@@ -6,16 +5,15 @@ class Download:
     def __init__(self):
         pass
 
-    def download_jpg(self,url,path,nome):
-        full_patch = patch + nome + '.jpg'
-        urllib.request.urlretrieve(url,full_patch)
+    def download_jpg(self,url,nome):
+        a = requests.get(url)
+        arquivo = nome+'.jpg'
+        with open( arquivo, 'wb') as f:
+            f.write(a.content)
         
 
 if __name__ == '__main__':
     url = input("Digite a url: ")
     nome = input("Nome do arquivo: ")
-    patch = "Documentos/"
-    
 
-    Download().download_jpg(url,patch,nome)
-    
+    Download().download_jpg(url,nome)
